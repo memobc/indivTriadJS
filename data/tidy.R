f355360f0c802679491f844512179648938e1166
Kyle Kurkela <kurkela@bc.edu>
2022-11-07 16:16:50 -0500

    analysis code

---
+++ data/tidy.R
@@ -0,0 +1,84 @@
+# tidy the triads data
+
+# requirements
+library(tidyverse)
+
+# load data
+data.files <- list.files(pattern = '.*experiment_data.csv')
+df <- map_dfr(.x = data.files, .f = read_csv)
+write_csv(x = df, file = 'raw.csv')
+
+# person, place stimuli lists
+stim <- read_csv('../experiment_data.csv')
+
+## retrieval
+
+df %>%
+  filter(phase == 'ret') -> onlyRetData.df
+
+onlyRetData.df %>%
+  filter(!is.na(stimulus)) %>%
+  select(where(~!all(is.na(.x)))) %>% 
+  select(rt, response, trial_index, subject) -> responseData.df
+
+onlyRetData.df %>%
+  filter(is.na(stimulus)) %>%
+  select(where(~!all(is.na(.x)))) %>% 
+  select(trial_index, subject, key:resp_opt_6) -> optionsData.df
+
+left_join(responseData.df, optionsData.df) -> joinedRetData.df
+
+joinedRetData.df %>%
+  mutate(correctResponse = map)
+
+# encoding ----------------------------------------------------------------
+
+df %>%
+  filter(phase == 'enc') -> onlyEncData.df
+
+onlyEncData.df %>%
+  select(where(~!all(is.na(.x)))) %>%
+  select(-stimulus) %>%
+  select(subject, trial_index, key, objOne, objTwo) %>%
+  mutate(trial_index = trial_index + 1) -> usefulEnc.df
+
+df %>%
+  filter(trial_type == 'html-slider-response') %>%
+  select(where(~!all(is.na(.x)))) %>%
+  select(subject, rt, response, trial_index) -> tidy.enc.df
+
+left_join(usefulEnc.df, tidy.enc.df) %>%
+  rename(success = response) -> tidy.enc.df
+
+write_csv(x = tidy.enc.df, file = 'tidy_enc.csv')
+
+## Ret + Enc
+
+findCorrectAnswer <- function(x){
+  objOne <- x$objOne
+  objTwo <- x$objTwo
+  resp_opts <- x %>% select(resp_opt_1:resp_opt_6) %>% as.list()
+  correctResponse <- which(resp_opts %in% objOne | resp_opts %in% objTwo)
+  if(length(correctResponse) > 1){
+    correctResponse <- NA
+  }
+  return(correctResponse)
+}
+
+# calculate isCorrect and correctResponse
+left_join(joinedRetData.df, usefulEnc.df, by = c('subject', 'key')) %>%
+  filter(!is.na(objOne)) %>%
+  group_by(subject, trial_index) %>%
+  nest() %>%
+  mutate(correctResponse = map_int(.x = data, .f = findCorrectAnswer)) %>%
+  unnest(data) %>%
+  mutate(isCorrect = response == correctResponse) -> tidy.df
+
+# calculate key type
+tidy.df %>%
+  mutate(keyType = case_when(key %in% stim$people ~ 'famous person',
+                             key %in% stim$place ~ 'everyday place',
+                             TRUE ~ '')) %>%
+  mutate(keyType = factor(keyType)) -> tidy.df
+
+write_csv(x = tidy.df, file = 'tidy_ret.csv')
