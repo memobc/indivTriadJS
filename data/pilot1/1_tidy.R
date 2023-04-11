4838e584b22c579c16e7ae3801750b68b91894d7
Kyle Kurkela <kurkela@bc.edu>
2023-04-11 16:20:10 -0400

    adding comments

--- data/pilot1/1_tidy.R
+++ data/pilot1/1_tidy.R
@@ -1,13 +1,16 @@
 # tidy the triads data
 
-# requirements
-library(tidyverse)
-source('findCorrectAnswer.R')
+# requirements ------------------------------------------------------------
 
-# load data
+library(tidyverse)
+library(assertthat)
+source('toolbox/findCorrectAnswer.R')
+
+# load data ---------------------------------------------------------------
+
 data.files <- list.files(path = 'pilot1', pattern = '.*experiment_data.csv', full.names = T)
-df <- map_dfr(.x = data.files, .f = read_csv)
-write_csv(x = df, file = 'pilot1/raw_concatenated.csv')
+df         <- map_dfr(.x = data.files, .f = read_csv)
+write_csv(x = df, file = 'pilot1/1_raw_concatenated.csv')
 
 people <- read_csv('../stim/famous_people.csv') %>%
         mutate(last = str_replace_na(last, '')) %>%
@@ -16,6 +19,7 @@ people <- read_csv('../stim/famous_people.csv') %>%
 places <- read_csv('../stim/common_places.csv')
 
 # retrieval ---------------------------------------------------------------
+# select subset of data corresponding to retrieval
 
 df %>%
   filter(phase == 'ret') %>%
@@ -29,8 +33,8 @@ left_join(choices.df, response.df) %>%
   select(-phase) -> RetData.df
 
 # encoding ----------------------------------------------------------------
+# select subset of data that corresponds to encoding
 
-# subset only encoding data
 df %>%
   filter(phase == 'enc') -> onlyEncData.df
 
@@ -50,39 +54,56 @@ left_join(usefulEnc.df, tidy.enc.df, by = c('subject', 'trial_index'), suffix = 
   rename(success = response) %>%
   select(-phase) -> tidy.enc.df
 
-write_csv(x = tidy.enc.df, file = 'pilot1/tidy_enc.csv')
+write_csv(x = tidy.enc.df, file = 'pilot1/1_tidy_enc.csv')
 
 # Knitting Enc + Ret together ---------------------------------------------
+# The problem with pilot 1 was that I did not create a column that matches
+# the encoding and retrieval trials to one another. Here we need to do that
+# manually. Another bug was that, for some trials and for subjects, there 
+# was accidentally doubles of some stimuli added to the experiment.
 
 RetData.df %>% 
   add_column(trial_index_enc = NA) -> RetData.df
 
+# for each row...
 for(i in 1:nrow(RetData.df)){
 
+  # the current row
   RetData.df %>% slice(i) -> rowN
 
+  # this subject encoding data
   tidy.enc.df %>%
     filter(subject == rowN$subject) -> enc_thisS
 
+  # try and match the retrieval key in this row to any of the encoding stimuli
   str_detect(enc_thisS$key, rowN$key) -> keyMatchVector 
   str_detect(enc_thisS$objOne, rowN$key) -> objOneMatchVector
   str_detect(enc_thisS$objTwo, rowN$key) -> objTwoMatchVector
 
-  assertthat::assert_that(sum(keyMatchVector) < 2, msg = 'More Then One Match')
+  # BUG FIX -- some trials had multiple matches at encoding. Should not be the case.
+  assert_that(sum(keyMatchVector) < 2, msg = 'More Than One Match')
+
   if(sum(objOneMatchVector < 2)){
+
     # see which of the two matches is in the response options
-    rowN %>% select(starts_with('resp_opt')) %>% as_vector() -> resp_options
-    enc_thisS %>% filter(objOneMatchVector) -> TheMatches
-    
-    TheMatches %>% filter(TheMatches$objTwo %in% resp_options) -> CorrectMatch
-    
+    rowN %>% 
+      select(starts_with('resp_opt')) %>% 
+      as_vector() -> resp_options
+
+    enc_thisS %>% 
+      filter(objOneMatchVector) -> TheMatches
+
+    TheMatches %>% 
+      filter(TheMatches$objTwo %in% resp_options) -> CorrectMatch
+
     # overwrite the match vector
     enc_thisS$trial_index %in% CorrectMatch$trial_index -> objOneMatchVector
-    
+
   }
-  assertthat::assert_that(sum(objOneMatchVector) < 2, msg = 'More Then One Match')
-  assertthat::assert_that(sum(objTwoMatchVector) < 2, msg = 'More Then One Match')
-  
+
+  assert_that(sum(objOneMatchVector) < 2, msg = 'More Than One Match')
+  assert_that(sum(objTwoMatchVector) < 2, msg = 'More Than One Match')
+
   if(any(keyMatchVector)){
     RetData.df$trial_index_enc[i] <- enc_thisS$trial_index[keyMatchVector]
   }
@@ -90,11 +111,11 @@ for(i in 1:nrow(RetData.df)){
   if(any(objOneMatchVector)){
     RetData.df$trial_index_enc[i] <- enc_thisS$trial_index[objOneMatchVector]
   }
-  
+
   if(any(objTwoMatchVector)){
     RetData.df$trial_index_enc[i] <- enc_thisS$trial_index[objTwoMatchVector]
   }
-  
+
 }
 
 left_join(RetData.df, tidy.enc.df,
@@ -112,4 +133,4 @@ joinedRetData.df %>%
   ungroup() %>%
   rename(trial_index_ret = trial_index) -> tidy.df
 
-write_csv(x = tidy.df, file = 'pilot1/tidy_ret.csv')
+write_csv(x = tidy.df, file = 'pilot1/1_tidy_ret.csv')
