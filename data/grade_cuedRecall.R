709bd7dd09b182712ce91fa3a9ba6476697db037
Kyle Kurkela <kurkela@bc.edu>
2023-04-11 14:04:33 -0400

    changed grade cued recall threshold for fuzzy matching

--- data/grade_cuedRecall.R
+++ data/grade_cuedRecall.R
@@ -7,19 +7,25 @@ grade_cuedRecall <- function(x){
   } else {
     objOneCorrect <- any(agrepl(pattern = x$objOne, responses, max.distance = list(all = 0.3), fixed = TRUE, ignore.case = TRUE))
   }
-  
+
   if(x$key_ret == x$objTwo){
     objTwoCorrect <- NA
   } else {
     objTwoCorrect <- any(agrepl(pattern = x$objTwo, responses, max.distance = list(all = 0.3), fixed = TRUE, ignore.case = TRUE))
   }
-  
+
   if(x$key_ret == x$key_enc){
     keyCorrect <- NA
   } else {
-    keyCorrect <- any(agrepl(pattern = x$key_enc, responses, max.distance = list(all = 0.3), fixed = TRUE, ignore.case = TRUE))
+
+    if(x$keyType_enc == 'famous place'){
+      keyCorrect <- any(agrepl(pattern = x$key_enc, responses, max.distance = list(all = 0.65), fixed = TRUE, ignore.case = TRUE))
+    } else {
+      keyCorrect <- any(agrepl(pattern = x$key_enc, responses, max.distance = list(all = 0.3), fixed = TRUE, ignore.case = TRUE))
+    }
+
   }
-  
+
   return(tibble(objOneCorrect, objTwoCorrect, keyCorrect))
 
 }
